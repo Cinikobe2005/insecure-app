792a4afaf151c313206b95eb3a8f3d9cc418c0d8
Cinikobe2005 <adedayoabdulqadri@gmail.com>
2025-03-27 05:37:26 -0700

    first commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import NavBar from "./NavBar";
+import HeroSection from "./HeroSection";
+import Section2 from "./Section2";
+import Section3 from "./Section3";
+import Footer from "./Footer";
+
+const Layout = () => {
+  return (
+    <div>
+      <NavBar />
+      <HeroSection />
+      <Section2 />
+      <Section3 />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
