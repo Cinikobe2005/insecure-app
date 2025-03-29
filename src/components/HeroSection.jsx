dc103d1162dbaf211bf44f042781692d752a6440
olorunwa-web <olorunwamoliki@gmail.com>
2025-03-29 14:38:17 +0100

    update herosection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -46,36 +46,6 @@ const HeroSection = () => {
 
        </section>
       </main>
-       {/* <main className = 'bg-[#2b2237] relative   w-full h-[80vh] md:h-[73vh]'>
-         <div className = 'hidden md:block absolute top-0 right-0 w-80 md:w-60 lg:w-80 z-10'>
-            <img src= {rightdesktop} alt="" className = 'w-[100%]'/>
-         </div> 
-         <div className = 'block md:hidden '>
-            <img src={leftmobile} alt=""/>
-         </div>
-         <div className = 'hidden md:block absolute top-90 left-0 w-40 md:w-30 lg:w-40'>
-           <img src= {leftimagedesktop} alt=""  clasName = 'w-[100%]'/>
-         </div>
-         <div className = 'block md:hidden absolute top-100 right-0 translate-middle '>
-           <img src= {rightimagedesktop} alt=""/>
-         </div>
-         <section className = 'container '>
-           <div className = 'md:px-6 sm:px-4 lg:px-30 absolute top-35 md:top-70 start-0 md:w-[50%] md:-translate-y-1/2 '>
-             <div className = 'text-center md:text-left'>
-             <div className = 'px-5 md:px-0'>
-              <h1 className = 'font-primary font-[400] text-[hsl(0,0%,98%)] text-5xl md:text-[34px] lg:text-5xl w-[100%] md:w-[90%] lg:w-[90%] pb-4'>Humanizing your insurance.</h1>
-              <p className = 'font-secondary text-[15px] font-[400] text-[hsl(0,0%,98%)] py-3'>Get your life insurance coverage easier and faster. We blend our expertise and technology to help you find the plan that's right for you. Ensure you and your loved ones are protected.</p>
-              <div className = 'py-3'>
-                <button className = 'font-secondary text-[15px] font-[400] text-[hsl(0,0%,98%)] border-1 border-solid border-[hsl(0,0%,98%)] rounded-[3px] w-[120px] md:w-[140px] h-[40px]  hover:bg-[hsl(0,0%,98%)] hover:text-[#2b2237]'>VIEW PLANS</button>
-              </div>
-             </div>
-             </div>
-           </div>
-            <div className = 'hidden md:block absolute top-35 md:right-6 lg:right-30 md:w-90 lg:w-100'>
-             <img src= {imagedesktop} alt="" clasName = '  w-[100%]'/>
-            </div>
-         </section>
-      </main> */}
          <div className = 'block md:hidden w-[100%]'>
                <img src= {imagemobile} alt="" className = 'w-full h-auto'/>
          </div>
